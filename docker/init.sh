ea78350bc77d18802ba10918dc2fa1d7fb466c2c
Tim Goudriaan <tim@codedmonkey.com>
2024-05-28 00:13:12 +0200

    Fix database creation command

--- docker/init.sh
+++ docker/init.sh
@@ -9,7 +9,7 @@ pg_ctl start -D /var/lib/postgresql/data &
 # Temporary wait for database todo
 while ! nc -z localhost 5432; do sleep 1; done;
 
-bin/console doctrine:database:create --no-ansi --no-interaction
+bin/console doctrine:database:create --if-not-exists --no-ansi --no-interaction
 bin/console doctrine:schema:update --complete --force --no-ansi --no-interaction
 bin/console messenger:setup-transports --no-ansi --no-interaction
 
