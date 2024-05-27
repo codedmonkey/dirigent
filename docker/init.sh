6dd19e4d4780051a8449e98b59d89f4029bd0a63
Tim Goudriaan <tim@codedmonkey.com>
2024-05-28 00:12:41 +0200

    Add temporary timeout before interacting with database

--- docker/init.sh
+++ docker/init.sh
@@ -6,6 +6,9 @@ composer run-script --no-ansi --no-interaction auto-scripts
 
 pg_ctl start -D /var/lib/postgresql/data &
 
+# Temporary wait for database todo
+while ! nc -z localhost 5432; do sleep 1; done;
+
 bin/console doctrine:database:create --no-ansi --no-interaction
 bin/console doctrine:schema:update --complete --force --no-ansi --no-interaction
 bin/console messenger:setup-transports --no-ansi --no-interaction
