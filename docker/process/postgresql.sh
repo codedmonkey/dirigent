91e0ae3b2ad9fb2f6fc79cb84eae50cac4676b70
Tim Goudriaan <tim@codedmonkey.com>
2024-07-23 16:48:58 +0200

    Add graceful PostgreSQL shutdown

--- docker/process/postgresql.sh
+++ docker/process/postgresql.sh
@@ -2,9 +2,16 @@
 
 set -e
 
+function shutdown()
+{
+    pkill pg_ctl
+}
+
 if [ ! -d "/srv/data/postgresql" ]; then
   mkdir -p /srv/data/postgresql
   initdb /srv/data/postgresql
 fi
+
+trap shutdown HUP INT QUIT ABRT KILL ALRM TERM TSTP
 
 exec pg_ctl start -D /srv/data/postgresql
