1116ea64b853be8947804d4896799e2f5dd48c71
Tim Goudriaan <tim@codedmonkey.com>
2024-11-11 23:53:44 +0100

    Code style fix

--- tests/Package/PackageVcsRepositoryValidatorTest.php
+++ tests/Package/PackageVcsRepositoryValidatorTest.php
@@ -35,7 +35,7 @@ class PackageVcsRepositoryValidatorTest extends TestCase
                     '%FOO',
                     '$BAR',
                 ],
-            ]
+            ],
         ];
     }
 
