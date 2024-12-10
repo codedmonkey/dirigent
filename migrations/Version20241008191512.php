38e488b8d0cdd4dc09ccf0e94729a5fa2fa3117d
Tim Goudriaan <tim@codedmonkey.com>
2024-12-10 22:26:03 +0100

    Deprecate broken migrations

--- migrations/Version20241008191512.php
+++ migrations/Version20241008191512.php
@@ -7,6 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
+/** @deprecated */
 final class Version20241008191512 extends AbstractMigration
 {
     public function getDescription(): string
@@ -16,11 +17,11 @@ final class Version20241008191512 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE package ADD type VARCHAR(255) DEFAULT NULL');
+        // $this->addSql('ALTER TABLE package ADD type VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE package DROP type');
+        // $this->addSql('ALTER TABLE package DROP type');
     }
 }
--- migrations/Version20241008192029.php
+++ migrations/Version20241008192029.php
@@ -7,6 +7,7 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
+/** @deprecated */
 final class Version20241008192029 extends AbstractMigration
 {
     public function getDescription(): string
@@ -16,11 +17,11 @@ final class Version20241008192029 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE credentials ADD token VARCHAR(255) DEFAULT NULL');
+        // $this->addSql('ALTER TABLE credentials ADD token VARCHAR(255) DEFAULT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE credentials DROP token');
+        // $this->addSql('ALTER TABLE credentials DROP token');
     }
 }
