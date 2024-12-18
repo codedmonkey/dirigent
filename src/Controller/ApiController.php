codedmonkey/conductor

Add package fetch strategy field

src/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace CodedMonkey\Conductor\Controller;
 
 use CodedMonkey\Conductor\Attribute\IsGrantedAccess;
 use CodedMonkey\Conductor\Doctrine\Entity\Package;
+use CodedMonkey\Conductor\Doctrine\Entity\PackageFetchStrategy;
 use CodedMonkey\Conductor\Doctrine\Repository\PackageRepository;
 use CodedMonkey\Conductor\Doctrine\Repository\VersionRepository;
 use CodedMonkey\Conductor\Message\TrackInstallations;
@@ -157,10 +158,12 @@ class ApiController extends AbstractController
 
     private function findPackage(string $packageName): ?Package
     {
+        // Search for the package in the database
         if (null !== $package = $this->packageRepository->findOneBy(['name' => $packageName])) {
             return $package;
         }
 
+        // Attempt to find a package from external registries
         if (null === $registry = $this->metadataResolver->findPackageProvider($packageName)) {
             return null;
         }
@@ -168,6 +171,7 @@ class ApiController extends AbstractController
         $package = new Package();
         $package->setName($packageName);
         $package->setMirrorRegistry($registry);
+        $package->setFetchStrategy(PackageFetchStrategy::Mirror);
 
         $this->packageRepository->save($package, true);
 

src/Controller/Dashboard/DashboardPackagesController.php
@@ -4,6 +4,7 @@ namespace CodedMonkey\Conductor\Controller\Dashboard;
 
 use CodedMonkey\Conductor\Attribute\IsGrantedAccess;
 use CodedMonkey\Conductor\Doctrine\Entity\Package;
+use CodedMonkey\Conductor\Doctrine\Entity\PackageFetchStrategy;
 use CodedMonkey\Conductor\Doctrine\Repository\PackageRepository;
 use CodedMonkey\Conductor\Doctrine\Repository\VersionRepository;
 use CodedMonkey\Conductor\EasyAdmin\PackagePaginator;
@@ -28,7 +29,6 @@ class DashboardPackagesController extends AbstractController
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
         private readonly PackageRepository $packageRepository,
-        private readonly VersionRepository $versionRepository,
         private readonly PackageMetadataResolver $metadataResolver,
         private readonly MessageBusInterface $messenger,
         private readonly AdminUrlGenerator $adminUrlGenerator,
@@ -135,6 +135,7 @@ class DashboardPackagesController extends AbstractController
                 $package = new Package();
                 $package->setName($packageName);
                 $package->setMirrorRegistry($registry);
+                $package->setFetchStrategy(PackageFetchStrategy::Mirror);
 
                 $this->packageRepository->save($package, true);
 

src/Doctrine/DataFixtures/PackageFixtures.php
@@ -3,6 +3,7 @@
 namespace CodedMonkey\Conductor\Doctrine\DataFixtures;
 
 use CodedMonkey\Conductor\Doctrine\Entity\Package;
+use CodedMonkey\Conductor\Doctrine\Entity\PackageFetchStrategy;
 use CodedMonkey\Conductor\Package\PackageMetadataResolver;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -21,6 +22,7 @@ class PackageFixtures extends Fixture
 
             $package->setName($packageData['name']);
             $package->setRepositoryUrl($packageData['repositoryUrl']);
+            $package->setFetchStrategy(PackageFetchStrategy::Vcs);
 
             $manager->persist($package);
             $manager->flush();

src/Doctrine/Entity/PackageFetchStrategy.php
@@ -1,0 +1,9 @@
+<?php
+
+namespace CodedMonkey\Conductor\Doctrine\Entity;
+
+enum PackageFetchStrategy: string
+{
+    case Mirror = 'mirror';
+    case Vcs = 'vcs';
+}

src/Form/PackageAddVcsFormType.php
@@ -4,6 +4,7 @@ namespace CodedMonkey\Conductor\Form;
 
 use CodedMonkey\Conductor\Doctrine\Entity\Credentials;
 use CodedMonkey\Conductor\Doctrine\Entity\Package;
+use CodedMonkey\Conductor\Doctrine\Entity\PackageFetchStrategy;
 use CodedMonkey\Conductor\Package\PackageVcsRepositoryValidator;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
@@ -16,6 +17,11 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PackageAddVcsFormType extends AbstractType
 {
+    public function __construct(
+        private readonly PackageVcsRepositoryValidator $vcsRepositoryValidator,
+    ) {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -35,11 +41,12 @@ class PackageAddVcsFormType extends AbstractType
         /** @var Package $package */
         $package = $event->getData();
 
-        $validator = new PackageVcsRepositoryValidator();
-        $validationResult = $validator->validate($package);
+        $package->setFetchStrategy(PackageFetchStrategy::Vcs);
+
+        $validationResult = $this->vcsRepositoryValidator->validate($package);
 
         if (null === $validationResult['error']) {
-            $validator->loadResult($package, $validationResult);
+            $this->vcsRepositoryValidator->loadResult($package, $validationResult);
         } else {
             $form->get('repositoryUrl')->addError(new FormError($validationResult['error']));
         }

src/Form/PackageFormType.php
@@ -4,6 +4,7 @@ namespace CodedMonkey\Conductor\Form;
 
 use CodedMonkey\Conductor\Doctrine\Entity\Credentials;
 use CodedMonkey\Conductor\Doctrine\Entity\Package;
+use CodedMonkey\Conductor\Doctrine\Entity\PackageFetchStrategy;
 use CodedMonkey\Conductor\Doctrine\Entity\Registry;
 use CodedMonkey\Conductor\Doctrine\Repository\RegistryRepository;
 use CodedMonkey\Conductor\Package\PackageVcsRepositoryValidator;
@@ -11,6 +12,8 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Event\PostSetDataEvent;
 use Symfony\Component\Form\Event\SubmitEvent;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormError;
@@ -56,7 +59,6 @@ class PackageFormType extends AbstractType
             $form
                 ->add('repositoryUrl', TextType::class, [
                     'disabled' => true,
-                    'data' => $package->getDefaultVersion()->getSource()['url'] ?? null,
                     'help' => 'The repository URL is automatically retrieved from the mirror registry. Remove the mirror registry first.',
                 ])
                 ->add('mirrorRegistry', EntityType::class, [
@@ -67,6 +69,14 @@ class PackageFormType extends AbstractType
                             ->where('registry.id = :id')
                             ->setParameter('id', $package->getMirrorRegistry()->getId());
                     },
+                ])
+                ->add('fetchStrategy', EnumType::class, [
+                    'class' => PackageFetchStrategy::class,
+                    'expanded' => true,
+                    'disabled' => !$package->getRepositoryUrl(),
+                    'choice_label' => function (PackageFetchStrategy $choice): string {
+                        return "package.fetch_strategy.{$choice->value}";
+                    },
                 ]);
         }
     }
@@ -76,10 +86,6 @@ class PackageFormType extends AbstractType
         $form = $event->getForm();
         /** @var Package $package */
         $package = $event->getData();
-
-        if (null === $package->getRepositoryUrl()) {
-
-        }
 
         $validationResult = $this->vcsRepositoryValidator->validate($package);
 

src/Package/PackageMetadataResolver.php
@@ -5,6 +5,7 @@ namespace CodedMonkey\Conductor\Package;
 use cebe\markdown\GithubMarkdown;
 use CodedMonkey\Conductor\Composer\ComposerClient;
 use CodedMonkey\Conductor\Doctrine\Entity\Package;
+use CodedMonkey\Conductor\Doctrine\Entity\PackageFetchStrategy;
 use CodedMonkey\Conductor\Doctrine\Entity\Registry;
 use CodedMonkey\Conductor\Doctrine\Entity\RegistryPackageMirroring;
 use CodedMonkey\Conductor\Doctrine\Entity\SuggestLink;
@@ -55,14 +56,10 @@ readonly class PackageMetadataResolver
 
     public function resolve(Package $package): void
     {
-        if (null !== $package->getMirrorRegistry()) {
-            $this->resolveRegistryPackage($package);
-        } elseif (null !== $package->getRepositoryUrl()) {
-            $this->resolveVcsPackage($package);
-        } else {
-            // todo resolve from other sources
-            throw new \LogicException();
-        }
+        match ($package->getFetchStrategy()) {
+            PackageFetchStrategy::Mirror => $this->resolveRegistryPackage($package),
+            PackageFetchStrategy::Vcs => $this->resolveVcsPackage($package),
+        };
 
         $this->messenger->dispatch(new DumpPackageProvider($package->getId()));
     }
@@ -105,6 +102,21 @@ readonly class PackageMetadataResolver
 
     private function resolveVcsPackage(Package $package): void
     {
+        if ($package->getMirrorRegistry()) {
+            $this->resolveVcsRepository($package);
+        }
+
+        if (!$package->getRepositoryUrl()) {
+            if ($package->getMirrorRegistry()) {
+                // todo log fallback to mirror registry
+
+                $this->resolveRegistryPackage($package);
+                return;
+            }
+
+            throw new \LogicException("No repository URL provided for {$package->getName()}.");
+        }
+
         $repository = $this->composer->createVcsRepository($package);
 
         $driver = $repository->getDriver();
@@ -120,6 +132,18 @@ readonly class PackageMetadataResolver
         $composerPackages = $repository->findPackages($packageName);
 
         $this->updatePackage($package, $composerPackages, $driver);
+    }
+
+    private function resolveVcsRepository(Package $package): void
+    {
+        $repository = $this->composer->createComposerRepository($package->getMirrorRegistry());
+        $composerPackages = $repository->findPackages($package->getName());
+
+        foreach ($composerPackages as $composerPackage) {
+            if ($composerPackage->isDefaultBranch()) {
+                $package->setRepositoryUrl($composerPackage->getSourceUrl());
+            }
+        }
     }
 
     /**
@@ -234,6 +258,7 @@ readonly class PackageMetadataResolver
         }
 
         if ($data->isDefaultBranch()) {
+            $package->setRepositoryUrl($data->getSourceUrl());
             $package->setDescription($description);
             $package->setType($this->sanitize($data->getType()));
             if ($data->isAbandoned() && !$package->isAbandoned()) {
@@ -316,6 +341,8 @@ readonly class PackageMetadataResolver
 
         if ($driver) {
             $this->updateReadme($version, $driver);
+        } else {
+            $version->setReadme(null);
         }
 
         $em->persist($version);

codedmonkey/dirigent

src/Controller/ApiController.php
@@ -4,6 +4,7 @@ namespace CodedMonkey\Dirigent\Controller;
 
 use CodedMonkey\Dirigent\Attribute\IsGrantedAccess;
 use CodedMonkey\Dirigent\Doctrine\Entity\Package;
+use CodedMonkey\Dirigent\Doctrine\Entity\PackageFetchStrategy;
 use CodedMonkey\Dirigent\Doctrine\Repository\PackageRepository;
 use CodedMonkey\Dirigent\Doctrine\Repository\VersionRepository;
 use CodedMonkey\Dirigent\Message\TrackInstallations;
@@ -157,10 +158,12 @@ class ApiController extends AbstractController
 
     private function findPackage(string $packageName): ?Package
     {
+        // Search for the package in the database
         if (null !== $package = $this->packageRepository->findOneBy(['name' => $packageName])) {
             return $package;
         }
 
+        // Attempt to find a package from external registries
         if (null === $registry = $this->metadataResolver->findPackageProvider($packageName)) {
             return null;
         }
@@ -168,6 +171,7 @@ class ApiController extends AbstractController
         $package = new Package();
         $package->setName($packageName);
         $package->setMirrorRegistry($registry);
+        $package->setFetchStrategy(PackageFetchStrategy::Mirror);
 
         $this->packageRepository->save($package, true);
 

src/Controller/Dashboard/DashboardPackagesController.php
@@ -4,8 +4,8 @@ namespace CodedMonkey\Dirigent\Controller\Dashboard;
 
 use CodedMonkey\Dirigent\Attribute\IsGrantedAccess;
 use CodedMonkey\Dirigent\Doctrine\Entity\Package;
+use CodedMonkey\Dirigent\Doctrine\Entity\PackageFetchStrategy;
 use CodedMonkey\Dirigent\Doctrine\Repository\PackageRepository;
-use CodedMonkey\Dirigent\Doctrine\Repository\VersionRepository;
 use CodedMonkey\Dirigent\EasyAdmin\PackagePaginator;
 use CodedMonkey\Dirigent\Form\PackageAddMirroringFormType;
 use CodedMonkey\Dirigent\Form\PackageAddVcsFormType;
@@ -28,7 +28,6 @@ class DashboardPackagesController extends AbstractController
     public function __construct(
         private readonly EntityManagerInterface $entityManager,
         private readonly PackageRepository $packageRepository,
-        private readonly VersionRepository $versionRepository,
         private readonly PackageMetadataResolver $metadataResolver,
         private readonly MessageBusInterface $messenger,
         private readonly AdminUrlGenerator $adminUrlGenerator,
@@ -135,6 +134,7 @@ class DashboardPackagesController extends AbstractController
                 $package = new Package();
                 $package->setName($packageName);
                 $package->setMirrorRegistry($registry);
+                $package->setFetchStrategy(PackageFetchStrategy::Mirror);
 
                 $this->packageRepository->save($package, true);
 

src/Doctrine/DataFixtures/PackageFixtures.php
@@ -3,6 +3,7 @@
 namespace CodedMonkey\Dirigent\Doctrine\DataFixtures;
 
 use CodedMonkey\Dirigent\Doctrine\Entity\Package;
+use CodedMonkey\Dirigent\Doctrine\Entity\PackageFetchStrategy;
 use CodedMonkey\Dirigent\Package\PackageMetadataResolver;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
@@ -21,6 +22,7 @@ class PackageFixtures extends Fixture
 
             $package->setName($packageData['name']);
             $package->setRepositoryUrl($packageData['repositoryUrl']);
+            $package->setFetchStrategy(PackageFetchStrategy::Vcs);
 
             $manager->persist($package);
             $manager->flush();

src/Doctrine/Entity/PackageFetchStrategy.php
@@ -1,0 +1,9 @@
+<?php
+
+namespace CodedMonkey\Dirigent\Doctrine\Entity;
+
+enum PackageFetchStrategy: string
+{
+    case Mirror = 'mirror';
+    case Vcs = 'vcs';
+}

src/Form/PackageAddVcsFormType.php
@@ -4,6 +4,7 @@ namespace CodedMonkey\Dirigent\Form;
 
 use CodedMonkey\Dirigent\Doctrine\Entity\Credentials;
 use CodedMonkey\Dirigent\Doctrine\Entity\Package;
+use CodedMonkey\Dirigent\Doctrine\Entity\PackageFetchStrategy;
 use CodedMonkey\Dirigent\Package\PackageVcsRepositoryValidator;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
@@ -16,6 +17,11 @@ use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class PackageAddVcsFormType extends AbstractType
 {
+    public function __construct(
+        private readonly PackageVcsRepositoryValidator $vcsRepositoryValidator,
+    ) {
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
@@ -35,11 +41,12 @@ class PackageAddVcsFormType extends AbstractType
         /** @var Package $package */
         $package = $event->getData();
 
-        $validator = new PackageVcsRepositoryValidator();
-        $validationResult = $validator->validate($package);
+        $package->setFetchStrategy(PackageFetchStrategy::Vcs);
+
+        $validationResult = $this->vcsRepositoryValidator->validate($package);
 
         if (null === $validationResult['error']) {
-            $validator->loadResult($package, $validationResult);
+            $this->vcsRepositoryValidator->loadResult($package, $validationResult);
         } else {
             $form->get('repositoryUrl')->addError(new FormError($validationResult['error']));
         }

src/Form/PackageFormType.php
@@ -4,6 +4,7 @@ namespace CodedMonkey\Dirigent\Form;
 
 use CodedMonkey\Dirigent\Doctrine\Entity\Credentials;
 use CodedMonkey\Dirigent\Doctrine\Entity\Package;
+use CodedMonkey\Dirigent\Doctrine\Entity\PackageFetchStrategy;
 use CodedMonkey\Dirigent\Doctrine\Entity\Registry;
 use CodedMonkey\Dirigent\Doctrine\Repository\RegistryRepository;
 use CodedMonkey\Dirigent\Package\PackageVcsRepositoryValidator;
@@ -11,6 +12,7 @@ use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Event\PostSetDataEvent;
 use Symfony\Component\Form\Event\SubmitEvent;
+use Symfony\Component\Form\Extension\Core\Type\EnumType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\FormError;
@@ -56,7 +58,6 @@ class PackageFormType extends AbstractType
             $form
                 ->add('repositoryUrl', TextType::class, [
                     'disabled' => true,
-                    'data' => $package->getDefaultVersion()->getSource()['url'] ?? null,
                     'help' => 'The repository URL is automatically retrieved from the mirror registry. Remove the mirror registry first.',
                 ])
                 ->add('mirrorRegistry', EntityType::class, [
@@ -66,6 +67,14 @@ class PackageFormType extends AbstractType
                         return $repository->createQueryBuilder('registry')
                             ->where('registry.id = :id')
                             ->setParameter('id', $package->getMirrorRegistry()->getId());
+                    },
+                ])
+                ->add('fetchStrategy', EnumType::class, [
+                    'class' => PackageFetchStrategy::class,
+                    'expanded' => true,
+                    'disabled' => !$package->getRepositoryUrl(),
+                    'choice_label' => function (PackageFetchStrategy $choice): string {
+                        return "package.fetch_strategy.{$choice->value}";
                     },
                 ]);
         }

src/Package/PackageMetadataResolver.php
@@ -5,6 +5,7 @@ namespace CodedMonkey\Dirigent\Package;
 use cebe\markdown\GithubMarkdown;
 use CodedMonkey\Dirigent\Composer\ComposerClient;
 use CodedMonkey\Dirigent\Doctrine\Entity\Package;
+use CodedMonkey\Dirigent\Doctrine\Entity\PackageFetchStrategy;
 use CodedMonkey\Dirigent\Doctrine\Entity\Registry;
 use CodedMonkey\Dirigent\Doctrine\Entity\RegistryPackageMirroring;
 use CodedMonkey\Dirigent\Doctrine\Entity\SuggestLink;
@@ -55,14 +56,10 @@ readonly class PackageMetadataResolver
 
     public function resolve(Package $package): void
     {
-        if (null !== $package->getMirrorRegistry()) {
-            $this->resolveRegistryPackage($package);
-        } elseif (null !== $package->getRepositoryUrl()) {
-            $this->resolveVcsPackage($package);
-        } else {
-            // todo resolve from other sources
-            throw new \LogicException();
-        }
+        match ($package->getFetchStrategy()) {
+            PackageFetchStrategy::Mirror => $this->resolveRegistryPackage($package),
+            PackageFetchStrategy::Vcs => $this->resolveVcsPackage($package),
+        };
 
         $this->messenger->dispatch(new DumpPackageProvider($package->getId()));
     }
@@ -105,6 +102,22 @@ readonly class PackageMetadataResolver
 
     private function resolveVcsPackage(Package $package): void
     {
+        if ($package->getMirrorRegistry()) {
+            $this->resolveVcsRepository($package);
+        }
+
+        if (!$package->getRepositoryUrl()) {
+            if ($package->getMirrorRegistry()) {
+                // todo log fallback to mirror registry
+
+                $this->resolveRegistryPackage($package);
+
+                return;
+            }
+
+            throw new \LogicException("No repository URL provided for {$package->getName()}.");
+        }
+
         $repository = $this->composer->createVcsRepository($package);
 
         $driver = $repository->getDriver();
@@ -120,6 +133,18 @@ readonly class PackageMetadataResolver
         $composerPackages = $repository->findPackages($packageName);
 
         $this->updatePackage($package, $composerPackages, $driver);
+    }
+
+    private function resolveVcsRepository(Package $package): void
+    {
+        $repository = $this->composer->createComposerRepository($package->getMirrorRegistry());
+        $composerPackages = $repository->findPackages($package->getName());
+
+        foreach ($composerPackages as $composerPackage) {
+            if ($composerPackage->isDefaultBranch()) {
+                $package->setRepositoryUrl($composerPackage->getSourceUrl());
+            }
+        }
     }
 
     /**
@@ -234,6 +259,7 @@ readonly class PackageMetadataResolver
         }
 
         if ($data->isDefaultBranch()) {
+            $package->setRepositoryUrl($data->getSourceUrl());
             $package->setDescription($description);
             $package->setType($this->sanitize($data->getType()));
             if ($data->isAbandoned() && !$package->isAbandoned()) {
@@ -316,6 +342,8 @@ readonly class PackageMetadataResolver
 
         if ($driver) {
             $this->updateReadme($version, $driver);
+        } else {
+            $version->setReadme(null);
         }
 
         $em->persist($version);