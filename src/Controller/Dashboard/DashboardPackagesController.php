codedmonkey/conductor

Add Edit package page

src/Controller/Dashboard/DashboardPackagesController.php
@@ -7,10 +7,12 @@ use CodedMonkey\Conductor\Doctrine\Entity\Package;
 use CodedMonkey\Conductor\Doctrine\Repository\PackageRepository;
 use CodedMonkey\Conductor\Doctrine\Repository\VersionRepository;
 use CodedMonkey\Conductor\EasyAdmin\PackagePaginator;
-use CodedMonkey\Conductor\Form\PackageAddMirroringType;
-use CodedMonkey\Conductor\Form\PackageAddVcsType;
+use CodedMonkey\Conductor\Form\PackageAddMirroringFormType;
+use CodedMonkey\Conductor\Form\PackageAddVcsFormType;
+use CodedMonkey\Conductor\Form\PackageFormType;
 use CodedMonkey\Conductor\Message\UpdatePackage;
 use CodedMonkey\Conductor\Package\PackageMetadataResolver;
+use Composer\Semver\VersionParser;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\PaginatorDto;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
@@ -63,28 +65,14 @@ class DashboardPackagesController extends AbstractController
         $package = $this->packageRepository->findOneBy(['name' => $packageName]);
 
         $versions = $package->getVersions()->toArray();
+        $latestVersion = $package->getDefaultVersion();
 
         usort($versions, Package::class . '::sortVersions');
 
-        // load the default branch version as it is used to display the latest available source.* and homepage info
-        $latestVersion = reset($versions);
-        foreach ($versions as $v) {
-            if ($v->isDefaultBranch()) {
-                $latestVersion = $v;
-                break;
-            }
-        }
-
         if (null !== $packageVersion) {
-            $version = $this->versionRepository->findOneBy(['package' => $package, 'version' => $packageVersion]);
+            $version = $package->getVersion((new VersionParser())->normalize($packageVersion));
         } else {
-            $version = $latestVersion;
-            foreach ($versions as $candidate) {
-                if (!$candidate->isDevelopment()) {
-                    $version = $candidate;
-                    break;
-                }
-            }
+            $version = $package->getLatestVersion();
         }
 
         return $this->render('dashboard/packages/package_info.html.twig', [
@@ -113,7 +101,7 @@ class DashboardPackagesController extends AbstractController
     #[IsGranted('ROLE_ADMIN')]
     public function addMirror(Request $request): Response
     {
-        $form = $this->createForm(PackageAddMirroringType::class);
+        $form = $this->createForm(PackageAddMirroringFormType::class);
 
         $form->handleRequest($request);
 
@@ -174,7 +162,7 @@ class DashboardPackagesController extends AbstractController
     #[IsGranted('ROLE_ADMIN')]
     public function addVcsRepository(Request $request): Response
     {
-        $form = $this->createForm(PackageAddVcsType::class);
+        $form = $this->createForm(PackageAddVcsFormType::class);
 
         $form->handleRequest($request);
 
@@ -189,6 +177,32 @@ class DashboardPackagesController extends AbstractController
         }
 
         return $this->render('dashboard/packages/add_vcs.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/dashboard/packages/edit/{packageName}', name: 'dashboard_packages_edit', requirements: ['packageName' => '[a-z0-9_.-]+/[a-z0-9_.-]+'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function edit(Request $request, string $packageName): Response
+    {
+        $package = $this->packageRepository->findOneByName($packageName);
+
+        $form = $this->createForm(PackageFormType::class, $package);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Package $package */
+            $package = $form->getData();
+            $this->packageRepository->save($package, true);
+
+            $this->messenger->dispatch(new UpdatePackage($package->getId()));
+
+            return $this->redirect($this->adminUrlGenerator->setRoute('dashboard_packages_info', ['packageName' => $package->getName()])->generateUrl());
+        }
+
+        return $this->render('dashboard/packages/package_edit.html.twig', [
+            'package' => $package,
             'form' => $form,
         ]);
     }

src/Form/PackageFormType.php
@@ -1,0 +1,99 @@
+<?php
+
+namespace CodedMonkey\Conductor\Form;
+
+use CodedMonkey\Conductor\Doctrine\Entity\Credentials;
+use CodedMonkey\Conductor\Doctrine\Entity\Package;
+use CodedMonkey\Conductor\Doctrine\Entity\Registry;
+use CodedMonkey\Conductor\Doctrine\Repository\RegistryRepository;
+use CodedMonkey\Conductor\Package\PackageVcsRepositoryValidator;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PostSetDataEvent;
+use Symfony\Component\Form\Event\SubmitEvent;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PackageFormType extends AbstractType
+{
+    public function __construct(
+        private readonly PackageVcsRepositoryValidator $vcsRepositoryValidator,
+    ) {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'disabled' => true,
+                'help' => 'Editing the package name is not possible, it\'s automatically retrieved from the source code.',
+            ])
+            ->add('repositoryUrl', TextType::class)
+            ->add('repositoryCredentials', EntityType::class, [
+                'label' => 'Credentials',
+                'required' => false,
+                'class' => Credentials::class,
+                'placeholder' => 'No credentials',
+            ])
+            ->add('mirrorRegistry', TextType::class, [
+                'disabled' => true,
+                'help' => 'Adding a mirror registry to a package is not possible. Delete the package first.',
+            ])
+            ->addEventListener(FormEvents::POST_SET_DATA, [$this, 'onPostSetData'])
+            ->addEventListener(FormEvents::SUBMIT, [$this, 'onSubmit']);
+    }
+
+    public function onPostSetData(PostSetDataEvent $event): void
+    {
+        $form = $event->getForm();
+        /** @var Package $package */
+        $package = $event->getData();
+
+        if ($package->getMirrorRegistry()) {
+            $form
+                ->add('repositoryUrl', TextType::class, [
+                    'disabled' => true,
+                    'data' => $package->getDefaultVersion()->getSource()['url'] ?? null,
+                    'help' => 'The repository URL is automatically retrieved from the mirror registry. Remove the mirror registry first.',
+                ])
+                ->add('mirrorRegistry', EntityType::class, [
+                    'class' => Registry::class,
+                    'required' => false,
+                    'query_builder' => function (RegistryRepository $repository) use ($package) {
+                        return $repository->createQueryBuilder('registry')
+                            ->where('registry.id = :id')
+                            ->setParameter('id', $package->getMirrorRegistry()->getId());
+                    },
+                ]);
+        }
+    }
+
+    public function onSubmit(SubmitEvent $event): void
+    {
+        $form = $event->getForm();
+        /** @var Package $package */
+        $package = $event->getData();
+
+        if (null === $package->getRepositoryUrl()) {
+
+        }
+
+        $validationResult = $this->vcsRepositoryValidator->validate($package);
+
+        if (null === $validationResult['error']) {
+            $this->vcsRepositoryValidator->loadResult($package, $validationResult);
+        } else {
+            $form->get('repositoryUrl')->addError(new FormError($validationResult['error']));
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Package::class,
+        ]);
+    }
+}

codedmonkey/dirigent

src/Controller/Dashboard/DashboardPackagesController.php
@@ -7,10 +7,12 @@ use CodedMonkey\Dirigent\Doctrine\Entity\Package;
 use CodedMonkey\Dirigent\Doctrine\Repository\PackageRepository;
 use CodedMonkey\Dirigent\Doctrine\Repository\VersionRepository;
 use CodedMonkey\Dirigent\EasyAdmin\PackagePaginator;
-use CodedMonkey\Dirigent\Form\PackageAddMirroringType;
-use CodedMonkey\Dirigent\Form\PackageAddVcsType;
+use CodedMonkey\Dirigent\Form\PackageAddMirroringFormType;
+use CodedMonkey\Dirigent\Form\PackageAddVcsFormType;
+use CodedMonkey\Dirigent\Form\PackageFormType;
 use CodedMonkey\Dirigent\Message\UpdatePackage;
 use CodedMonkey\Dirigent\Package\PackageMetadataResolver;
+use Composer\Semver\VersionParser;
 use Doctrine\ORM\EntityManagerInterface;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\PaginatorDto;
 use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
@@ -63,28 +65,14 @@ class DashboardPackagesController extends AbstractController
         $package = $this->packageRepository->findOneBy(['name' => $packageName]);
 
         $versions = $package->getVersions()->toArray();
+        $latestVersion = $package->getDefaultVersion();
 
         usort($versions, Package::class . '::sortVersions');
 
-        // load the default branch version as it is used to display the latest available source.* and homepage info
-        $latestVersion = reset($versions);
-        foreach ($versions as $v) {
-            if ($v->isDefaultBranch()) {
-                $latestVersion = $v;
-                break;
-            }
-        }
-
         if (null !== $packageVersion) {
-            $version = $this->versionRepository->findOneBy(['package' => $package, 'version' => $packageVersion]);
+            $version = $package->getVersion((new VersionParser())->normalize($packageVersion));
         } else {
-            $version = $latestVersion;
-            foreach ($versions as $candidate) {
-                if (!$candidate->isDevelopment()) {
-                    $version = $candidate;
-                    break;
-                }
-            }
+            $version = $package->getLatestVersion();
         }
 
         return $this->render('dashboard/packages/package_info.html.twig', [
@@ -113,7 +101,7 @@ class DashboardPackagesController extends AbstractController
     #[IsGranted('ROLE_ADMIN')]
     public function addMirror(Request $request): Response
     {
-        $form = $this->createForm(PackageAddMirroringType::class);
+        $form = $this->createForm(PackageAddMirroringFormType::class);
 
         $form->handleRequest($request);
 
@@ -174,7 +162,7 @@ class DashboardPackagesController extends AbstractController
     #[IsGranted('ROLE_ADMIN')]
     public function addVcsRepository(Request $request): Response
     {
-        $form = $this->createForm(PackageAddVcsType::class);
+        $form = $this->createForm(PackageAddVcsFormType::class);
 
         $form->handleRequest($request);
 
@@ -189,6 +177,32 @@ class DashboardPackagesController extends AbstractController
         }
 
         return $this->render('dashboard/packages/add_vcs.html.twig', [
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/dashboard/packages/edit/{packageName}', name: 'dashboard_packages_edit', requirements: ['packageName' => '[a-z0-9_.-]+/[a-z0-9_.-]+'])]
+    #[IsGranted('ROLE_ADMIN')]
+    public function edit(Request $request, string $packageName): Response
+    {
+        $package = $this->packageRepository->findOneByName($packageName);
+
+        $form = $this->createForm(PackageFormType::class, $package);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var Package $package */
+            $package = $form->getData();
+            $this->packageRepository->save($package, true);
+
+            $this->messenger->dispatch(new UpdatePackage($package->getId()));
+
+            return $this->redirect($this->adminUrlGenerator->setRoute('dashboard_packages_info', ['packageName' => $package->getName()])->generateUrl());
+        }
+
+        return $this->render('dashboard/packages/package_edit.html.twig', [
+            'package' => $package,
             'form' => $form,
         ]);
     }

src/Form/PackageFormType.php
@@ -1,0 +1,95 @@
+<?php
+
+namespace CodedMonkey\Dirigent\Form;
+
+use CodedMonkey\Dirigent\Doctrine\Entity\Credentials;
+use CodedMonkey\Dirigent\Doctrine\Entity\Package;
+use CodedMonkey\Dirigent\Doctrine\Entity\Registry;
+use CodedMonkey\Dirigent\Doctrine\Repository\RegistryRepository;
+use CodedMonkey\Dirigent\Package\PackageVcsRepositoryValidator;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Event\PostSetDataEvent;
+use Symfony\Component\Form\Event\SubmitEvent;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormEvents;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class PackageFormType extends AbstractType
+{
+    public function __construct(
+        private readonly PackageVcsRepositoryValidator $vcsRepositoryValidator,
+    ) {
+    }
+
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'disabled' => true,
+                'help' => 'Editing the package name is not possible, it\'s automatically retrieved from the source code.',
+            ])
+            ->add('repositoryUrl', TextType::class)
+            ->add('repositoryCredentials', EntityType::class, [
+                'label' => 'Credentials',
+                'required' => false,
+                'class' => Credentials::class,
+                'placeholder' => 'No credentials',
+            ])
+            ->add('mirrorRegistry', TextType::class, [
+                'disabled' => true,
+                'help' => 'Adding a mirror registry to a package is not possible. Delete the package first.',
+            ])
+            ->addEventListener(FormEvents::POST_SET_DATA, [$this, 'onPostSetData'])
+            ->addEventListener(FormEvents::SUBMIT, [$this, 'onSubmit']);
+    }
+
+    public function onPostSetData(PostSetDataEvent $event): void
+    {
+        $form = $event->getForm();
+        /** @var Package $package */
+        $package = $event->getData();
+
+        if ($package->getMirrorRegistry()) {
+            $form
+                ->add('repositoryUrl', TextType::class, [
+                    'disabled' => true,
+                    'data' => $package->getDefaultVersion()->getSource()['url'] ?? null,
+                    'help' => 'The repository URL is automatically retrieved from the mirror registry. Remove the mirror registry first.',
+                ])
+                ->add('mirrorRegistry', EntityType::class, [
+                    'class' => Registry::class,
+                    'required' => false,
+                    'query_builder' => function (RegistryRepository $repository) use ($package) {
+                        return $repository->createQueryBuilder('registry')
+                            ->where('registry.id = :id')
+                            ->setParameter('id', $package->getMirrorRegistry()->getId());
+                    },
+                ]);
+        }
+    }
+
+    public function onSubmit(SubmitEvent $event): void
+    {
+        $form = $event->getForm();
+        /** @var Package $package */
+        $package = $event->getData();
+
+        $validationResult = $this->vcsRepositoryValidator->validate($package);
+
+        if (null === $validationResult['error']) {
+            $this->vcsRepositoryValidator->loadResult($package, $validationResult);
+        } else {
+            $form->get('repositoryUrl')->addError(new FormError($validationResult['error']));
+        }
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Package::class,
+        ]);
+    }
+}