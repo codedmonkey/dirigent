bbcba40854f37e05c0e2de8fae24b7e86bf3502e
Tim Goudriaan <tim@codedmonkey.com>
2025-02-05 23:17:47 +0100

    Randomize kernel secret in the standalone image through a configuration file

--- docker/env.php
+++ docker/env.php
@@ -5,6 +5,7 @@ return [
     'DATABASE_URL' => 'postgresql://dirigent@127.0.0.1:5432/dirigent?serverVersion=16&charset=utf8',
     'DIRIGENT_IMAGE' => '1',
     'GITHUB_TOKEN' => '',
+    'KERNEL_SECRET_FILE' => '/srv/config/secrets/kernel_secret',
     'MAILER_DSN' => 'null://null',
     'MESSENGER_TRANSPORT_DSN' => 'doctrine://default?auto_setup=0',
     'SENTRY_DSN' => '',
