6aac10c3e9ebbb7fa89672f7b7f806f8f0c05bf9
Tim Goudriaan <tim@codedmonkey.com>
2025-02-26 21:32:25 +0100

    Add init scripts to Docker image

---
+++ tests/Docker/Standalone/PhpPlatformTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace CodedMonkey\Dirigent\Tests\Docker\Standalone;
+
+class PhpPlatformTest extends DockerStandaloneTestCase
+{
+    public function testComposerPlatformRequirements(): void
+    {
+        $this->assertCommandSuccessful(
+            ['composer', 'check-platform-reqs', '--no-dev'],
+            'Platform requirements of Composer packages must be met.',
+        );
+    }
+
+    public function testSymfonyCacheGenerated(): void
+    {
+        $this->assertContainerFileExists(
+            'var/cache/prod/CodedMonkey_Dirigent_KernelProdContainer.php',
+            'The Symfony cache must be generated during initialization.',
+        );
+    }
+}
