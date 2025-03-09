b431ae256a78b53985ebd81e8fcf802fc3b33915
Tim Goudriaan <tim@codedmonkey.com>
2025-03-09 17:48:55 +0100

    Add distribution builder

--- src/Package/PackageDistributionResolver.php
+++ src/Package/PackageDistributionResolver.php
@@ -3,22 +3,33 @@
 namespace CodedMonkey\Dirigent\Package;
 
 use CodedMonkey\Dirigent\Composer\ComposerClient;
+use CodedMonkey\Dirigent\Composer\ConfigFactory;
 use CodedMonkey\Dirigent\Doctrine\Entity\Version;
+use Composer\IO\NullIO;
+use Composer\Pcre\Preg;
+use Composer\Util\Filesystem as ComposerFilesystem;
+use Composer\Util\Git as GitUtility;
+use Composer\Util\ProcessExecutor;
+use Composer\Util\Url;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Filesystem\Filesystem;
 
 readonly class PackageDistributionResolver
 {
     private Filesystem $filesystem;
-    private string $storagePath;
+    private string $distributionStoragePath;
 
     public function __construct(
         private ComposerClient $composer,
+        #[Autowire(param: 'dirigent.dist_builder.enabled')]
+        private bool $buildDistributions,
+        #[Autowire(param: 'dirigent.dist_builder.dev_packages')]
+        private bool $buildDevDistributions,
         #[Autowire(param: 'dirigent.storage.path')]
         string $storagePath,
     ) {
         $this->filesystem = new Filesystem();
-        $this->storagePath = "$storagePath/distribution";
+        $this->distributionStoragePath = "$storagePath/distribution";
     }
 
     public function exists(string $packageName, string $versionName, string $reference, string $type): bool
@@ -28,7 +39,7 @@ readonly class PackageDistributionResolver
 
     public function path(string $packageName, string $versionName, string $reference, string $type): string
     {
-        return "{$this->storagePath}/{$packageName}/{$versionName}-{$reference}.{$type}";
+        return "$this->distributionStoragePath/$packageName/$versionName-$reference.$type";
     }
 
     public function resolve(Version $version, string $reference, string $type): bool
@@ -41,17 +52,76 @@ readonly class PackageDistributionResolver
             return true;
         }
 
-        if ($reference !== $version->getDistReference() || $type !== $version->getDistType()) {
+        if (
+            null === $version->getDist()
+            && $this->buildDistributions
+            && (!$version->isDevelopment() || $this->buildDevDistributions)
+        ) {
+            return $this->build($version, $reference, $type);
+        } elseif (null !== $version->getDist()) {
+            return $this->mirror($version, $reference, $type);
+        }
+
+        return false;
+    }
+
+    private function build(Version $version, string $reference, string $type): bool
+    {
+        // Skip building of outdated references for now
+        if ($reference !== $version->getSourceReference()) {
             return false;
         }
 
-        $distUrl = $version->getDistUrl();
-        $path = $this->path($packageName, $versionName, $reference, $type);
+        // Only provide .zip support for now
+        if ('zip' !== $type) {
+            return false;
+        }
 
-        $this->filesystem->mkdir(dirname($path));
+        $package = $version->getPackage();
+        $repositoryUrl = $package->getRepositoryUrl();
+        $distributionPath = $this->path($package->getName(), $version->getNormalizedVersion(), $reference, $type);
+
+        $composerConfig = ConfigFactory::createForVcsRepository($repositoryUrl, $package->getRepositoryCredentials());
+
+        $gitUtility = new GitUtility(
+            $io = new NullIO(),
+            $composerConfig,
+            $process = new ProcessExecutor($io),
+            new ComposerFilesystem($process),
+        );
+
+        $cacheRepositoryName = Preg::replace('{[^a-z0-9.]}i', '-', Url::sanitize($repositoryUrl));
+        $cachePath = $composerConfig->get('cache-vcs-dir') . '/' . $cacheRepositoryName . '/';
+
+        $this->filesystem->mkdir(dirname($distributionPath));
+
+        $gitUtility->runCommands([
+            ['git', 'archive', '--format=zip', "--output=$distributionPath", $reference],
+        ], $repositoryUrl, $cachePath);
+
+        return true;
+    }
+
+    private function mirror(Version $version, string $reference, string $type): bool
+    {
+        // Skip mirroring of outdated references for now
+        if ($reference !== $version->getDistReference()) {
+            return false;
+        }
+
+        // The distribution type must match the origin format
+        if ($type !== $version->getDistType()) {
+            return false;
+        }
+
+        $package = $version->getPackage();
+        $distributionUrl = $version->getDistUrl();
+        $distributionPath = $this->path($package->getName(), $version->getNormalizedVersion(), $reference, $type);
+
+        $this->filesystem->mkdir(dirname($distributionPath));
 
         $httpDownloader = $this->composer->createHttpDownloader();
-        $httpDownloader->copy($distUrl, $path);
+        $httpDownloader->copy($distributionUrl, $distributionPath);
 
         return true;
     }
