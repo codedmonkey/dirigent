add6c7eba990e2313b3e8810877aaf4d2b7ed171
Tim Goudriaan <tim@codedmonkey.com>
2025-04-09 00:38:06 +0200

    Refactor dashboard URLs in templates

--- src/Twig/DashboardExtension.php
+++ src/Twig/DashboardExtension.php
@@ -2,25 +2,15 @@
 
 namespace CodedMonkey\Dirigent\Twig;
 
-use EasyCorp\Bundle\EasyAdminBundle\Router\AdminUrlGenerator;
 use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\Extension\GlobalsInterface;
-use Twig\TwigFunction;
 
 class DashboardExtension extends AbstractExtension implements GlobalsInterface
 {
     public function __construct(
-        private readonly AdminUrlGenerator $adminUrlGenerator,
         private readonly ParameterBagInterface $parameterBag,
     ) {
-    }
-
-    public function getFunctions(): array
-    {
-        return [
-            new TwigFunction('dashboard_path', [$this, 'path']),
-        ];
     }
 
     public function getGlobals(): array
@@ -30,10 +20,5 @@ class DashboardExtension extends AbstractExtension implements GlobalsInterface
                 'slug' => $this->parameterBag->get('dirigent.slug'),
             ],
         ];
-    }
-
-    public function path(string $route, array $routeParams = []): string
-    {
-        return $this->adminUrlGenerator->setRoute($route, $routeParams)->generateUrl();
     }
 }
