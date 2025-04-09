e19ffdafb01005751d309c4c8d84281d2e847b37
Tim Goudriaan <tim@codedmonkey.com>
2025-04-10 01:45:44 +0200

    Add TOTP functionality
    Signed-off-by: Tim Goudriaan <tim@codedmonkey.com>

--- src/Controller/Dashboard/DashboardAccountController.php
+++ src/Controller/Dashboard/DashboardAccountController.php
@@ -6,8 +6,12 @@ use CodedMonkey\Dirigent\Doctrine\Entity\User;
 use CodedMonkey\Dirigent\Doctrine\Repository\UserRepository;
 use CodedMonkey\Dirigent\Form\AccountFormType;
 use CodedMonkey\Dirigent\Form\ChangePasswordFormType;
+use CodedMonkey\Dirigent\Form\MfaClearFormType;
+use CodedMonkey\Dirigent\Form\MfaSetupFormType;
+use Scheb\TwoFactorBundle\Security\TwoFactor\Provider\Totp\TotpAuthenticatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\FormInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
@@ -20,6 +24,7 @@ class DashboardAccountController extends AbstractController
     public function __construct(
         private readonly UserRepository $userRepository,
         private readonly UserPasswordHasherInterface $passwordHasher,
+        private readonly TotpAuthenticatorInterface $totpAuthenticator,
     ) {
     }
 
@@ -28,8 +33,6 @@ class DashboardAccountController extends AbstractController
     public function account(Request $request, #[CurrentUser] User $user): Response
     {
         $accountForm = $this->createForm(AccountFormType::class, $user);
-        $passwordForm = $this->createForm(ChangePasswordFormType::class);
-
         $accountForm->handleRequest($request);
 
         if ($accountForm->isSubmitted() && $accountForm->isValid()) {
@@ -40,14 +43,11 @@ class DashboardAccountController extends AbstractController
             return $this->redirectToRoute('dashboard_account');
         }
 
+        $passwordForm = $this->createForm(ChangePasswordFormType::class);
         $passwordForm->handleRequest($request);
 
         if ($passwordForm->isSubmitted()) {
-            $currentPassword = $passwordForm->get('currentPassword')->getData();
-
-            if (!$this->passwordHasher->isPasswordValid($user, $currentPassword)) {
-                $passwordForm->get('currentPassword')->addError(new FormError('Your current password is incorrect.'));
-            }
+            $this->validatePassword($passwordForm->get('currentPassword'), $user);
 
             if ($passwordForm->isValid()) {
                 $user->setPlainPassword($passwordForm->get('newPassword')->getData());
@@ -60,9 +60,86 @@ class DashboardAccountController extends AbstractController
             }
         }
 
-        return $this->render('dashboard/account.html.twig', [
+        return $this->render('dashboard/account/account.html.twig', [
             'accountForm' => $accountForm,
             'passwordForm' => $passwordForm,
         ]);
+    }
+
+    #[Route('/account/mfa', name: 'dashboard_account_mfa')]
+    #[IsGranted('ROLE_USER')]
+    public function mfa(Request $request, #[CurrentUser] User $user): Response
+    {
+        if ($user->isTotpAuthenticationEnabled()) {
+            $form = $this->createForm(MfaClearFormType::class);
+            $form->handleRequest($request);
+
+            if ($form->isSubmitted()) {
+                $this->validatePassword($form->get('currentPassword'), $user);
+
+                if ($form->isValid()) {
+                    $user->setTotpSecret(null);
+
+                    $this->userRepository->save($user, true);
+
+                    $this->addFlash('warning', 'Multi-factor authentication was successfully disabled.');
+
+                    return $this->redirectToRoute('dashboard_account');
+                }
+            }
+
+            return $this->render('dashboard/account/mfa_clear.html.twig', [
+                'form' => $form,
+            ]);
+        }
+
+        $session = $request->getSession();
+
+        if (null === $totpSecret = $session->get('totp_secret')) {
+            $totpSecret = $this->totpAuthenticator->generateSecret();
+
+            $session->set('totp_secret', $totpSecret);
+        }
+
+        $user->setTotpSecret($totpSecret);
+
+        $form = $this->createForm(MfaSetupFormType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted()) {
+            $totpCode = $form->get('totpCode')->getData();
+
+            if (!$this->totpAuthenticator->checkCode($user, $totpCode)) {
+                $user->setTotpSecret(null);
+
+                $form->get('totpCode')->addError(new FormError('account.error.totp-incorrect'));
+            }
+
+            $this->validatePassword($form->get('currentPassword'), $user);
+
+            if ($form->isValid()) {
+                $this->userRepository->save($user, true);
+
+                $this->addFlash('success', 'Multi-factor authentication was successfully enabled.');
+
+                $session->remove('totp_secret');
+
+                return $this->redirectToRoute('dashboard_account');
+            }
+        }
+
+        return $this->render('dashboard/account/mfa_setup.html.twig', [
+            'form' => $form,
+            'totpContent' => $this->totpAuthenticator->getQRContent($user),
+        ]);
+    }
+
+    private function validatePassword(FormInterface $passwordField, User $user): void
+    {
+        $currentPassword = $passwordField->getData();
+
+        if (!$this->passwordHasher->isPasswordValid($user, $currentPassword)) {
+            $passwordField->addError(new FormError('account.error.password-incorrect'));
+        }
     }
 }
