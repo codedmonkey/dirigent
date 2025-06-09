11d5dc087e7b61f747eee21b6d863fd3f1dae0d5
Tim Goudriaan <tim@codedmonkey.com>
2025-06-09 17:32:55 +0200

    Create functional test for deleting packages with TestEntityFactoryTrait

--- tests/FunctionalTests/WebTestCaseTrait.php
+++ tests/FunctionalTests/WebTestCaseTrait.php
@@ -8,12 +8,26 @@ use Symfony\Bundle\FrameworkBundle\KernelBrowser;
 
 trait WebTestCaseTrait
 {
+    /**
+     * @template TServiceClass of object
+     *
+     * @param class-string<TServiceClass> $class
+     *
+     * @return TServiceClass
+     */
+    protected function getService(string $class, ?string $name = null): object
+    {
+        /** @var KernelBrowser $client */
+        $client = static::getClient();
+
+        return $client->getContainer()->get($name ?: $class);
+    }
+
     protected function loginUser(string $username = 'user'): User
     {
         /** @var KernelBrowser $client */
         $client = static::getClient();
-        /** @var UserRepository $userRepository */
-        $userRepository = $client->getContainer()->get(UserRepository::class);
+        $userRepository = static::getService(UserRepository::class);
 
         /** @var User $user */
         $user = $userRepository->findOneByUsername($username);
