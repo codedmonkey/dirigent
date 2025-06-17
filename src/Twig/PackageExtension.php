0eb6d1bd0816652e659d6f18426833b30c7c5e1d
Tim Goudriaan <tim@codedmonkey.com>
2025-06-17 21:28:31 +0200

    Add package list filters

--- src/Twig/PackageExtension.php
+++ src/Twig/PackageExtension.php
@@ -5,23 +5,43 @@ namespace CodedMonkey\Dirigent\Twig;
 use CodedMonkey\Dirigent\Doctrine\Entity\Package;
 use CodedMonkey\Dirigent\Doctrine\Repository\PackageRepository;
 use Composer\Pcre\Preg;
+use Symfony\Component\HttpFoundation\RequestStack;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Twig\Attribute\AsTwigFunction;
+use Twig\Attribute\AsTwigTest;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigTest;
 
-class PackageExtension extends AbstractExtension
+readonly class PackageExtension
 {
     public function __construct(
-        private readonly PackageRepository $packageRepository,
+        private PackageRepository $packageRepository,
+        private UrlGeneratorInterface $router,
+        private RequestStack $requestStack,
     ) {
     }
 
-    public function getTests(): array
+    #[AsTwigFunction('packageFilterParameters')]
+    public function getFilterParameters(string $parameter, string|int|null $value): array
     {
-        return [
-            new TwigTest('existing_package', [$this, 'packageExistsTest']),
+        $request = $this->requestStack->getCurrentRequest();
+        $filters = [
+            'query' => $request->query->get('query'),
+            'registry' => $request->query->get('registry'),
         ];
+
+        $filters[$parameter] = $value;
+
+        return array_filter($filters, fn ($value) => null !== $value);
     }
 
+    #[AsTwigFunction('packageFilterUrl')]
+    public function createFilterUrl(string $parameter, string|int|null $value): string
+    {
+        return $this->router->generate('dashboard_packages', $this->getFilterParameters($parameter, $value));
+    }
+
+    #[AsTwigTest('existing_package')]
     public function packageExistsTest(mixed $package): bool
     {
         if ($package instanceof Package) {
