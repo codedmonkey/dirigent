2fe0166509b3828d416824e1ecbf400c6c91e031
Tim Goudriaan <tim@codedmonkey.com>
2025-07-03 20:48:54 +0200

    Refactor login path to pretty URL
    Signed-off-by: Tim Goudriaan <tim@codedmonkey.com>

---
+++ tests/FunctionalTests/Controller/Dashboard/DashboardSecurityControllerTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace CodedMonkey\Dirigent\Tests\FunctionalTests\Controller\Dashboard;
+
+use CodedMonkey\Dirigent\Tests\Helper\WebTestCaseTrait;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
+
+class DashboardSecurityControllerTest extends WebTestCase
+{
+    use WebTestCaseTrait;
+
+    /**
+     * Verify the homepage redirects to the login page when the application is not publicly accessible.
+     */
+    public function testLoginRedirect(): void
+    {
+        $client = static::createClient();
+        $client->request('GET', '/');
+
+        $this->assertResponseStatusCodeSame(Response::HTTP_FOUND);
+
+        $client->followRedirect();
+
+        self::assertSame('/login', $client->getRequest()->getRequestUri());
+    }
+}
