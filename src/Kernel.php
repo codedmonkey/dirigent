ee892baba2b4c6b2396d613c8c80a32ab8658212
Tim Goudriaan <tim@codedmonkey.com>
2025-10-17 02:19:03 +0200

    Update development version to 0.6
    Signed-off-by: Tim Goudriaan <tim@codedmonkey.com>

--- src/Kernel.php
+++ src/Kernel.php
@@ -14,7 +14,7 @@ class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
 
-    public const VERSION = '0.5.x-dev';
+    public const VERSION = '0.6.x-dev';
 
     protected function configureContainer(ContainerConfigurator $container): void
     {
