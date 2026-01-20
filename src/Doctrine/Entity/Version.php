6e0fec92ba59d72a341176dcbbe31567887899d5
Tim Goudriaan <tim@codedmonkey.com>
2026-01-20 13:26:49 +0100

    Create metadata entities
    Start of refactoring package metadata by separating package metadata from the version table to a new metadata table to create immutable packages
    
    Signed-off-by: Tim Goudriaan <tim@codedmonkey.com>

--- src/Doctrine/Entity/Version.php
+++ src/Doctrine/Entity/Version.php
@@ -138,7 +138,7 @@ class Version extends TrackedEntity implements \Stringable
         return "$this->name $this->version ($this->normalizedVersion)";
     }
 
-    public function getId(): int
+    public function getId(): ?int
     {
         return $this->id;
     }
