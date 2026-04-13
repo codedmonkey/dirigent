d048940c2dd112e31600cd3a72f1a5ca323227b9
Tim Goudriaan <tim@codedmonkey.com>
2026-04-13 13:57:00 +0200

    Fix packages not being dynamically mirrored on request from the API

--- tests/FunctionalTests/Controller/ApiControllerPublicTest.php
+++ tests/FunctionalTests/Controller/ApiControllerPublicTest.php
@@ -2,13 +2,21 @@
 
 namespace CodedMonkey\Dirigent\Tests\FunctionalTests\Controller;
 
+use CodedMonkey\Dirigent\Doctrine\Entity\Package;
+use CodedMonkey\Dirigent\Doctrine\Entity\Registry;
+use CodedMonkey\Dirigent\Doctrine\Entity\RegistryPackageMirroring;
 use CodedMonkey\Dirigent\Tests\FunctionalTests\PublicKernel;
+use CodedMonkey\Dirigent\Tests\Helper\KernelTestCaseTrait;
+use CodedMonkey\Dirigent\Tests\Helper\MockEntityFactoryTrait;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class ApiControllerPublicTest extends KernelTestCase
 {
+    use KernelTestCaseTrait;
+    use MockEntityFactoryTrait;
+
     #[\Override]
     protected static function getKernelClass(): string
     {
@@ -28,7 +36,7 @@ class ApiControllerPublicTest extends KernelTestCase
         ], $rootData);
     }
 
-    public function testPackage(): void
+    public function testPackagMetadata(): void
     {
         self::bootKernel();
 
@@ -37,11 +45,42 @@ class ApiControllerPublicTest extends KernelTestCase
         $this->assertNotSame([], $packageData);
     }
 
-    public function testPackageDev(): void
+    public function testPackageMetadataDev(): void
     {
         self::bootKernel();
 
         $packageData = $this->requestJson('/p2/psr/log~dev.json', 'GET');
+
+        $this->assertNotSame([], $packageData);
+    }
+
+    public function testPackageMetadataIsNotFound(): void
+    {
+        self::bootKernel();
+
+        $request = Request::create('/p2/psr/container.json', 'GET');
+        $response = self::$kernel->handle($request);
+
+        $this->assertSame(Response::HTTP_NOT_FOUND, $response->getStatusCode());
+    }
+
+    public function testPackageMetadataIsMirroredOnRequest(): void
+    {
+        self::bootKernel();
+
+        // Verify the package doesn't already exist
+        $package = $this->findEntity(Package::class, ['name' => 'psr/container']);
+
+        $this->assertNull($package);
+
+        // Update the registry so that it allows dynamically adding packages on request from the API
+        $registry = $this->findEntity(Registry::class, ['name' => 'Packagist']);
+        $registry->setPackageMirroring(RegistryPackageMirroring::Automatic);
+
+        $this->persistEntities($registry);
+
+        // Execute the API endpoint
+        $packageData = $this->requestJson('/p2/psr/container.json', 'GET');
 
         $this->assertNotSame([], $packageData);
     }
