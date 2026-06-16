cbab7b06e01f5c31dcbdd5ef2fe79c78f16e6f18
Tim Goudriaan <tim@codedmonkey.com>
2026-06-16 10:12:15 +0200

    Add async handling for resolving of distributions

---
+++ src/Message/ResolveDistributionHandler.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodedMonkey\Dirigent\Message;
+
+use CodedMonkey\Dirigent\Doctrine\Repository\MetadataRepository;
+use CodedMonkey\Dirigent\Package\PackageDistributionResolver;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler]
+readonly class ResolveDistributionHandler
+{
+    public function __construct(
+        private MetadataRepository $metadataRepository,
+        private PackageDistributionResolver $distributionResolver,
+    ) {
+    }
+
+    public function __invoke(ResolveDistribution $message): void
+    {
+        $metadata = $this->metadataRepository->find($message->metadataId);
+
+        $this->distributionResolver->resolve($metadata, $message->type, async: false);
+    }
+}
