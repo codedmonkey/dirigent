a0831c4421c80483157d1e6502c908569ffac2ba
Tim Goudriaan <tim@codedmonkey.com>
2026-06-16 10:22:33 +0200

    Refactor PackageFetchStrategy enum

---
+++ src/Entity/PackageFetchStrategy.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CodedMonkey\Dirigent\Entity;
+
+use Symfony\Contracts\Translation\TranslatableInterface;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+enum PackageFetchStrategy: string implements TranslatableInterface
+{
+    case Mirror = 'mirror';
+    case Vcs = 'vcs';
+
+    public function isMirror(): bool
+    {
+        return self::Mirror === $this;
+    }
+
+    public function isVcs(): bool
+    {
+        return self::Vcs === $this;
+    }
+
+    public function trans(TranslatorInterface $translator, ?string $locale = null): string
+    {
+        return $translator->trans("package.fetch-strategy.$this->value");
+    }
+}
