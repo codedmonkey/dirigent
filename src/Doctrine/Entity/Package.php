308a357a819624a13d157e96c31965388b9c6d58
Tim Goudriaan <tim@codedmonkey.com>
2026-07-14 15:31:23 +0200

    Make the fetch strategy column of packages required in the database

--- src/Doctrine/Entity/Package.php
+++ src/Doctrine/Entity/Package.php
@@ -65,8 +65,8 @@ class Package extends TrackedEntity
     #[ORM\Column(nullable: true)]
     private ?string $remoteId = null;
 
-    #[ORM\Column(nullable: true, enumType: PackageFetchStrategy::class)]
-    private ?PackageFetchStrategy $fetchStrategy = null;
+    #[ORM\Column(enumType: PackageFetchStrategy::class)]
+    private PackageFetchStrategy $fetchStrategy;
 
     #[ORM\ManyToOne]
     private ?Registry $mirrorRegistry = null;
@@ -240,10 +240,6 @@ class Package extends TrackedEntity
 
     public function getFetchStrategy(): PackageFetchStrategy
     {
-        if (!$this->fetchStrategy) {
-            return $this->mirrorRegistry ? PackageFetchStrategy::Mirror : PackageFetchStrategy::Source;
-        }
-
         return $this->fetchStrategy;
     }
 
