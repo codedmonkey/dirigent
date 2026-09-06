3494d20722cf355a39321db03a5e806593969ec1
Tim Goudriaan <tim@codedmonkey.com>
2026-09-06 21:11:00 +0200

    Use Symfony locks to prevent a distribution from being resolved multiple times at once

--- src/Package/PackageDistributionResolver.php
+++ src/Package/PackageDistributionResolver.php
@@ -11,6 +11,7 @@ use CodedMonkey\Dirigent\Doctrine\Repository\DistributionRepository;
 use CodedMonkey\Dirigent\Message\ResolveDistribution;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Lock\LockFactory;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Messenger\Stamp\TransportNamesStamp;
 
@@ -23,6 +24,7 @@ readonly class PackageDistributionResolver
         private MessageBusInterface $messenger,
         private ComposerClient $composer,
         private DistributionRepository $distributionRepository,
+        private LockFactory $lockFactory,
         #[Autowire(param: 'dirigent.distributions.dev_versions')]
         private bool $includeDevVersions,
         #[Autowire(param: 'dirigent.storage.path')]
@@ -73,23 +75,35 @@ readonly class PackageDistributionResolver
             return false;
         }
 
-        if (null === $distribution = $this->distributionRepository->findOneByMetadataAndType($metadata, $type)) {
-            $distribution = new Distribution($metadata, $type);
-        }
-
         $distributionUrl = $metadata->getDistributionUrl();
+        $path = $this->path($metadata, $type);
 
-        $this->filesystem->mkdir(dirname($path));
+        $lock = $this->lockFactory->createLock('distribution.' . hash('sha256', $path), ttl: null);
+        $lock->acquire(blocking: true);
 
-        $httpDownloader = $this->composer->createHttpDownloader();
-        $httpDownloader->copy($distributionUrl, $path);
+        try {
+            if ($this->fileExists($path)) {
+                return true;
+            }
 
-        $distribution->setSource($distributionUrl);
-        $distribution->setResolvedAt();
+            if (null === $distribution = $this->distributionRepository->findOneByMetadataAndType($metadata, $type)) {
+                $distribution = new Distribution($metadata, $type);
+            }
 
-        $this->distributionRepository->save($distribution, true);
+            $this->filesystem->mkdir(dirname($path));
 
-        return true;
+            $httpDownloader = $this->composer->createHttpDownloader();
+            $httpDownloader->copy($distributionUrl, $path);
+
+            $distribution->setSource($distributionUrl);
+            $distribution->setResolvedAt();
+
+            $this->distributionRepository->save($distribution, true);
+
+            return true;
+        } finally {
+            $lock->release();
+        }
     }
 
     private function fileExists(string $path): bool
